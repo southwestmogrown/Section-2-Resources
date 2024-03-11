f1de7418f595cc5e9f28e68e2aaeba1d591f0695
southwestmogrown <southwestmogrown@gmail.com>
2024-03-11 10:27:25 -0500

    adjust for new students

--- Module-6-Resources/week_17/D3/EOD/phones.py
+++ Module-6-Resources/week_17/D3/EOD/phones.py
@@ -31,31 +31,3 @@ phones = [
         "color": "stormy black"
     }
 ]
-
-def get_unique_models(phone_list):
-    just_models = map(lambda phone: phone["model"], phone_list)
-    print(list(set(just_models)))
-
-
-get_unique_models(phones)
-
-# def get_unique_models(phone_list):
-#     seen = []
-#     return filter(lambda phone: seen.append(phone['model']) is None if phone['model'] not in seen else False, phone_list)
-    
-#     # so what is going on here in the filter method???
-#     # think of the lambda statement being written like this...
-#     if phone['model'] not in seen:
-#         return seen.append(phone['model']) is None
-#     else:
-#         return False
-#     #
-#     # Because this is a callback in a filter, it needs to return True or False
-#     # to determine if the value gets added to our filtered list
-#     #
-#     # In the else, we return False, makes sense...
-#     # The append method does not have a return, but we know that if a
-#     # function does not have a set return value, it returns None!
-#     # So we check if the return value is None, which will evaluate to true!
-
-
