2597abb68bc0210fdd0adc3a4b3bb6afaffb7dfc
southwestmogrown <southwestmogrown@gmail.com>
2024-03-13 13:59:48 -0500

    query lecture

---
+++ Module-4-Resources/W10/D3/notes/instagram-demo/queries.sql
@@ -0,0 +1,30 @@
+-- SELECT * FROM users;
+
+-- SELECT username, email FROM users;
+
+-- SELECT image_url, caption, title
+-- FROM posts
+-- WHERE user_id = 1;
+
+-- SELECT caption, title
+-- FROM posts
+-- WHERE title LIKE '%Title' AND user_id = 1;
+
+-- SELECT caption, title
+-- FROM posts
+-- WHERE caption LIKE 'Another%' AND user_id = 1;
+
+-- SELECT caption, title
+-- FROM posts
+-- WHERE user_id IN (1,3,5);
+
+-- SELECT body 
+-- FROM comments
+-- WHERE post_id BETWEEN 4 AND 10;
+
+-- DELETE FROM posts
+-- WHERE id = 3;
+
+UPDATE users
+SET username = 'swilkey'
+WHERE id = 1;
