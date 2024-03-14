c8bb9592fcb35f9d3395bdaca882a637d88fa915
southwestmogrown <southwestmogrown@gmail.com>
2024-03-14 15:29:27 -0500

    joins lecture

--- Module-4-Resources/W10/D4/04-sql-join-one-to-many/sql-practice/step-1.sql
+++ Module-4-Resources/W10/D4/04-sql-join-one-to-many/sql-practice/step-1.sql
@@ -2,3 +2,7 @@
 -- JOIN the tables, matching FOREIGN KEYs to the corresponding PRIMARY KEY.
 -- Your code here 
 
+SELECT bands.name, albums.title
+FROM albums
+JOIN bands
+ON bands.id = albums.band_id;
