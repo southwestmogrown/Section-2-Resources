f3d0cf5fcc01d8a44f18a06eb75c1b3786a6c695
southwestmogrown <southwestmogrown@gmail.com>
2024-03-19 12:30:46 -0500

    models/seeders lecture

---
+++ Module-4-Resources/W11/D2/notes/sequelize-demo/db/seeders/20240319171840-users-seeds.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const { User } = require("../models");
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert(
+      "Users",
+      [
+        {
+          firstName: "John",
+          lastName: "Doe",
+          email: "john@doe.com",
+          password: "johnspassword",
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete("Users", null, {});
+  },
+};
