b214c4915757dbd485abad2be46dea64e5e61ce6
southwestmogrown <southwestmogrown@gmail.com>
2024-03-20 16:17:05 -0500

    relationship sp and lazy vs eager loading

--- Module-4-Resources/W11/D3/notes/bloostagram/routes/albums.js
+++ Module-4-Resources/W11/D3/notes/bloostagram/routes/albums.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const { Album, Image } = require("../db/models");
+const { Album, Image, AlbumImage } = require("../db/models");
 
 const router = express.Router();
 
@@ -10,6 +10,36 @@ router.get("/", async (req, res) => {
   });
 
   res.json(albums);
+});
+
+router.get("/lazy/:id", async (req, res) => {
+  const album = await Album.findByPk(req.params.id);
+
+  const images = await Image.findAll({
+    through: {
+      AlbumImage,
+      where: {
+        albumId: album.id,
+      },
+    },
+  });
+
+  const returnObj = {
+    album,
+    images,
+  };
+
+  res.json(returnObj);
+});
+
+router.get("/eager/:id", async (req, res) => {
+  const album = await Album.findByPk(req.params.id, {
+    include: {
+      model: Image,
+    },
+  });
+
+  res.json(album);
 });
 
 module.exports = router;
