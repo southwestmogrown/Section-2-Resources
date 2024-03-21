d84da7f1d3b58d86de1e208d31d3671ed4851169
southwestmogrown <southwestmogrown@gmail.com>
2024-03-21 11:50:42 -0500

    association methods

--- Module-4-Resources/W11/D4/notes/bloostagram/app.js
+++ Module-4-Resources/W11/D4/notes/bloostagram/app.js
@@ -3,12 +3,14 @@ require("dotenv").config();
 
 const usersRouter = require("./routes/users");
 const albumsRouter = require("./routes/albums");
+const aggregatesRouter = require("./routes/aggregates");
 
 const app = express();
 
 app.use(express.json());
 app.use("/users", usersRouter);
 app.use("/albums", albumsRouter);
+app.use("/aggregates", aggregatesRouter);
 
 const port = process.env.PORT;
 app.listen(port, () => console.log(`Listening on port ${process.env.PORT}...`));
