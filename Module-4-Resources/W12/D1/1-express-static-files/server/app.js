3a52cf5ef6853232043c2b43c2b48d2b4fe15d26
southwestmogrown <southwestmogrown@gmail.com>
2024-03-25 15:51:54 -0500

    serving static files

--- Module-4-Resources/W12/D1/1-express-static-files/server/app.js
+++ Module-4-Resources/W12/D1/1-express-static-files/server/app.js
@@ -1,7 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-// Your code here 
+// wrong =>  localhost:5000/stylesheets/assets/css/application.css
+// right => localhost:5000/stylesheets/application.css
+app.use("/stylesheets", express.static("assets/css/"));
+
+// app.use(express.static("assets"));
+
+app.use(express.static("assets/scripts"));
+
+// BONUS
+app.use("/stickers", express.static("assets/images"));
 
 const port = 5000;
-app.listen(port, () => console.log('Server is listening on port', port));
+app.listen(port, () => console.log("Server is listening on port", port));
--- Module-4-Resources/W12/D1/notes/bloostagram/app.js
+++ Module-4-Resources/W12/D1/notes/bloostagram/app.js
@@ -9,9 +9,16 @@ const app = express();
 
 app.use(express.json());
 
+app.use("/static", express.static("assets"));
+// app.use("/styling", express.static("/assets/css/index.css"));
+
 app.use("/users", usersRouter);
 app.use("/albums", albumsRouter);
 app.use("/aggregates", aggregatesRouter);
+
+// app.get("/assets/css/styles.css", (req, res) => {
+//   res.send("howdy");
+// });
 
 const port = process.env.PORT;
 app.listen(port, () => console.log(`Listening on port ${process.env.PORT}...`));
