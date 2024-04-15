a1d911fc6c9eb0d468c688228bc648d9b2f1860a
southwestmogrown <southwestmogrown@gmail.com>
2024-04-15 15:21:05 -0500

    class based components lecture

--- Module-5-Resources/lecture-code/W15/W15D1/react-dice/src/components/Die.jsx
+++ Module-5-Resources/lecture-code/W15/W15D1/react-dice/src/components/Die.jsx
@@ -1,19 +1,30 @@
-import { Component } from "react";
+// import { Component } from "react";
 import "./Die.css";
 
 // TODO: Refactor this component to be a functional component
 
-class Die extends Component {
-  render() {
-    return (
-      <div>
-        <i
-          className={`Die fas fa-dice-${this.props.face} 
-                    ${this.props.shake ? "shake-dice" : ""}`}
-        />
-      </div>
-    );
-  }
+// class Die extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <i
+//           className={`Die fas fa-dice-${this.props.face} 
+//                     ${this.props.shake ? "shake-dice" : ""}`}
+//         />
+//       </div>
+//     );
+//   }
+// }
+
+function Die(props) {
+  return (
+    <div>
+      <i
+        className={`Die fas fa-dice-${props.face} 
+                  ${props.shake ? "shake-dice" : ""}`}
+      />
+    </div>
+  );
 }
 
 export default Die;
