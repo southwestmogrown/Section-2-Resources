5dbc10b0eea8d985b45f873626a9760fac2d14c9
southwestmogrown <southwestmogrown@gmail.com>
2024-04-16 11:27:37 -0500

    store breakdown comments

--- Module-5-Resources/lecture-code/W15/W15D2/Bloostagram/store/store.js
+++ Module-5-Resources/lecture-code/W15/W15D2/Bloostagram/store/store.js
@@ -1,11 +1,37 @@
 import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import usersReducer from "./usersReducer";
+/*
+`combineReducers` combines all the reducer functions into one big reducer
+function, which is typically called `rootReducer`. This is the most important
+part of this file. You will add your reducers here to work with your various
+components.
+*/
+
+// create rootReducer here:
+// Your code here
 
 const rootReducer = combineReducers({
   usersState: usersReducer
 });
 
+/*
+`enhancer` allows you to alter the store and add functionality such as Redux
+DevTools and logger (similar to morgan) middleware.
+*/
+
 let enhancer;
+
+/*
+`compose` gives you the ability to add more than one enhancer to the store. Vite
+provides access to environment variables through `import.meta.env`.
+`import.meta.env.MODE` has 3 settings:
+   1. `npm run dev` makes `import.meta.env.MODE` = 'development'
+   2. `npm run build` makes `import.meta.env.MODE` = 'production'
+   3. `npm test` makes `import.meta.env.MODE` = 'test'
+
+window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ along with the Chrome extension for
+Redux DevTools will set up your Redux DevTools in the browser.
+*/
 
 if (import.meta.env.MODE !== "production") {
   const logger = (await import("redux-logger")).default;
@@ -15,6 +41,16 @@ if (import.meta.env.MODE !== "production") {
 
   enhancer = composeEnhancers(applyMiddleware(logger));
 }
+
+/*
+`configureStore` is the variable you will use in your root `main.jsx` to give
+the Redux store access to the full application.
+
+`createStore`: creates a store object literal ({}).
+`preloadedState`: initializes the state. This is mainly used for hydrating state
+  from the server. (This is not important right now.)
+`enhancer`: see above.
+*/
 
 const configureStore = (preLoadedState) => {
   return createStore(rootReducer, preLoadedState, enhancer);
