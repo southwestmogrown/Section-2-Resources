03fe6140d0f0c2ab004533b698087d88d4fcee6f
southwestmogrown <southwestmogrown@gmail.com>
2024-04-17 18:48:50 -0500

    debug bloostagram

--- Module-5-Resources/lecture-code/W15/W15D3/Bloostagram/src/components/Comment/Comment.jsx
+++ Module-5-Resources/lecture-code/W15/W15D3/Bloostagram/src/components/Comment/Comment.jsx
@@ -1,10 +1,7 @@
 import "./Comment.css";
 function Comment({ comment }) {
-  return (
-    <div className="comment-body">
-      <h4>{comment}</h4>
-    </div>
-  );
+  // console.log(comment);
+  return <div className="comment-body">{<h4>{comment.body}</h4>}</div>;
 }
 
 export default Comment;
--- Module-5-Resources/lecture-code/W15/W15D3/Bloostagram/src/components/Post/PostDetails.jsx
+++ Module-5-Resources/lecture-code/W15/W15D3/Bloostagram/src/components/Post/PostDetails.jsx
@@ -7,30 +7,33 @@ import { useSelector } from "react-redux";
 
 function PostDetails() {
   const { postId } = useParams();
-  const posts = useSelector((state) => state.postsState.posts);
+  const post = useSelector((state) => state.postsState.posts[postId]);
+  // const posts = useSelector((state) => state.postsState.posts);
 
-  const post = posts.find((post) => post.id === +postId);
+  // const post = posts.find((post) => post.id === +postId);
   console.log(post);
 
   return (
-    <div className="post-details-container">
-      <Link to="/posts">Back to feed...</Link>
-      <Post postData={post} />
-      <div>
-        <div className="likes-container">
-          <p>Likes: Coming soon...</p>
-          <button
-            className="like-btn"
-            onClick={() => alert("Like functionality coming soon...")}
-          >
-            Like
-          </button>
+    post && (
+      <div className="post-details-container">
+        <Link to="/posts">Back to feed...</Link>
+        <Post postData={post} />
+        <div>
+          <div className="likes-container">
+            <p>Likes: Coming soon...</p>
+            <button
+              className="like-btn"
+              onClick={() => alert("Like functionality coming soon...")}
+            >
+              Like
+            </button>
+          </div>
+          {post.Comments.map((comment, i) => {
+            return <Comment key={i} comment={comment} />;
+          })}
         </div>
-        {post.Comments.map((comment, i) => {
-          return <Comment key={i} comment={comment} />;
-        })}
       </div>
-    </div>
+    )
   );
 }
 
