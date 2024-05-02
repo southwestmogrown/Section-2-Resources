5415b2b64070e5ae88fcf87a4e38c940e2d86b29
southwestmogrown <southwestmogrown@gmail.com>
2024-05-02 13:17:22 -0500

    python basic class syntax lecture

--- Module-6-Resources/week_17/D4/Short-Practices/02-decorators/problems/04-chain-decorator.py
+++ Module-6-Resources/week_17/D4/Short-Practices/02-decorators/problems/04-chain-decorator.py
@@ -3,7 +3,7 @@
 # argument as a callback, implement two inner wrapper functions, and finally
 # return the wrapper function object in each respective wrapper function.
 
-# Implement the inner wrapper function with the following:
+# Implement the outer wrapper function with the following:
 # - Takes a variable number of positional and keyword arguments
 # - Initializes a variable that calls the callback function with arguments
 #   passed to the wrapper
@@ -18,7 +18,30 @@
 # Finally, be sure to decorate num function using the decorator syntax.
 
 # Write your function here.
+def chain_decorator(func):
+    def outer_wrapper(*args, **kwargs):
+        def inner_wrapper(*args, **kwargs):
+            x = func(*args, **kwargs)
+            return x * 3
+        x = inner_wrapper(*args, **kwargs)
+        return x * x
+    return outer_wrapper
 
+def square_decorator(func):
+    def wrapper(a, b):
+        x = func(a, b)
+        return x ** 2
+    return wrapper
+
+def multiply_by_three(func):
+    def wrapper(a, b):
+        x = func(a, b)
+        return x * 3
+    return wrapper
+
+@chain_decorator
+@square_decorator
+@multiply_by_three
 def num(a, b):
     return a + b
 
