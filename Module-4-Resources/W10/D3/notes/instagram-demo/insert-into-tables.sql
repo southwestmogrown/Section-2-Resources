a48f6b6e66d7b8cf6227b44d30d9b72961e95235
southwestmogrown <southwestmogrown@gmail.com>
2024-06-05 12:27:28 -0500

    create-drop-tables

---
+++ Module-4-Resources/W10/D3/notes/instagram-demo/insert-into-tables.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (username, email, hashedPassword, bio, profilePic)
+VALUES ("shanew", "shane@wilkey.com", "23084329058u34k", "I'm pretty okay.", "someurl.com"),
+       ("brirob", "briana@robinson.com", "23084329058u34k", "Best instructor around!", "someurl.com"),
+       ("shanes''s demo", "briana@robinson.com", "23084329058u34k", "Best instructor around!", "someurl.com");
