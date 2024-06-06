e485da0bd05f8e575a3e7b5cb7430a3f6694161e
southwestmogrown <southwestmogrown@gmail.com>
2024-06-06 10:43:54 -0500

    morning relationship lecture

--- Module-4-Resources/W10/D4/notes/bloostagram/create-tables.sql
+++ Module-4-Resources/W10/D4/notes/bloostagram/create-tables.sql
@@ -32,13 +32,15 @@ CREATE TABLE posts (
     caption TEXT,
     title VARCHAR(150),
     created_at DATE NOT NULL,
-    updated_at DATE NOT NULL
+    updated_at DATE NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (image_id) REFERENCES images(id)
 );
 
 CREATE TABLE comments (
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    user_id INTEGER,
-    post_id INTEGER,
+    user_id INTEGER REFERENCES users(id),
+    post_id INTEGER REFERENCES posts(id),
     body TEXT,
     created_at DATE NOT NULL,
     updated_at DATE NOT NULL
@@ -46,15 +48,15 @@ CREATE TABLE comments (
 
 CREATE TABLE likes (
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    user_id INTEGER,
-    post_id INTEGER,
+    user_id INTEGER REFERENCES users(id),
+    post_id INTEGER REFERENCES posts(id),
     created_at DATE NOT NULL,
     updated_at DATE NOT NULL
 );
 
 CREATE TABLE albums (
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    user_id INTEGER,
+    user_id INTEGER REFERENCES users(id),
     name VARCHAR(100)
 );
 
