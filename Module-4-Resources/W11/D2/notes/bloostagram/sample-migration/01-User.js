8dc431006eea96b41af8afbf8a339fc51215b35a
southwestmogrown <southwestmogrown@gmail.com>
2024-06-11 11:46:27 -0500

    update package.json for versioning issues

---
+++ Module-4-Resources/W11/D2/notes/bloostagram/sample-migration/01-User.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     */
+    await queryInterface.createTable("Users", {
+      id: Sequelize.INTEGER,
+      username: Sequelize.STRING,
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     */
+    await queryInterface.dropTable("Users");
+  },
+};
