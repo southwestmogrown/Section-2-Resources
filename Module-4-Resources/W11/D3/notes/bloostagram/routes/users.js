5afbb62d35c0d37556dfc8a9cf005e426b69a12f
southwestmogrown <southwestmogrown@gmail.com>
2024-06-12 10:38:59 -0500

    morning query lecture

--- Module-4-Resources/W11/D3/notes/bloostagram/routes/users.js
+++ Module-4-Resources/W11/D3/notes/bloostagram/routes/users.js
@@ -1,4 +1,75 @@
 const express = require("express");
+const { Op } = require("sequelize");
 const router = express.Router();
+
+const { User } = require("../db/models");
+
+// router.get("/", async (req, res) => {
+//   const allUsers = await User.findAll();
+
+//   res.json(allUsers);
+// });
+
+// router.get("/", async (req, res) => {
+//   const allUsers = await User.findAll({
+//     attributes: ["id", "username"],
+//   });
+
+//   res.json(allUsers);
+// });
+
+// router.get("/", async (req, res) => {
+//   const allUsers = await User.findAll({
+//     attributes: { exclude: ["id", "username"] },
+//   });
+
+//   res.json(allUsers);
+// });
+
+// router.get("/", async (req, res) => {
+//   const allUsers = await User.findAll({
+//     attributes: ["id", "username"],
+//     where: {
+//       username: "bloo",
+//     },
+//   });
+
+//   res.json(allUsers);
+// });
+
+router.get("/", async (req, res) => {
+  const allUsers = await User.findAll({
+    attributes: ["id", "username"],
+    where: {
+      id: { [Op.in]: [2, 3] },
+    },
+    order: [
+      ["id", "ASC"],
+      ["username", "DESC"],
+    ],
+  });
+
+  res.json(allUsers);
+});
+
+// router.get("/:username", async (req, res) => {
+//   const user = await User.findOne({
+//     attributes: ["id", "username"],
+//     where: {
+//       username: req.params.username,
+//     },
+//   });
+
+//   res.json(user);
+// });
+
+router.get("/:userId", async (req, res) => {
+  const id = req.params.userId;
+  const user = await User.findByPk(id, {
+    attributes: ["id", "username"],
+  });
+
+  res.json(user);
+});
 
 module.exports = router;
