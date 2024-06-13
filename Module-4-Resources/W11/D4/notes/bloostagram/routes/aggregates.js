eff68532f903bda2c3793461eecfcec1a3125311
southwestmogrown <southwestmogrown@gmail.com>
2024-06-13 12:22:51 -0500

    add slides for w11d4

--- Module-4-Resources/W11/D4/notes/bloostagram/routes/aggregates.js
+++ Module-4-Resources/W11/D4/notes/bloostagram/routes/aggregates.js
@@ -9,6 +9,48 @@ const {
   Like,
   Album,
   AlbumImage,
+  Rating,
 } = require("../db/models");
+
+router.get("/min", async (req, res) => {
+  const min = await Rating.min("rating");
+
+  res.json(min);
+});
+router.get("/max", async (req, res) => {
+  const max = await Rating.max("rating");
+
+  res.json(max);
+});
+
+router.get("/count", async (req, res) => {
+  const count = await Rating.count();
+
+  res.json(count);
+});
+router.get("/sum", async (req, res) => {
+  const sum = await Rating.sum("rating");
+
+  res.json(sum);
+});
+
+router.get("/:imageId/average", async (req, res) => {
+  const sum = await Rating.sum("rating", {
+    where: {
+      imageId: req.params.imageId,
+    },
+  });
+  const image = await Image.findByPk(req.params.imageId);
+  const ratings = await image.getRatings();
+
+  const average = sum / ratings.length;
+
+  const imageObj = {
+    ...image.toJSON(),
+    averageRating: average.toFixed(2),
+  };
+
+  res.json(imageObj);
+});
 
 module.exports = router;
