8257cd21fd6e5b237cbc94db1cd1182518ec7eb7
southwestmogrown <southwestmogrown@gmail.com>
2024-06-17 15:13:15 -0500

    serving static files/w12d1 slides

--- Module-4-Resources/W12/D1/notes/bloostagram/app.js
+++ Module-4-Resources/W12/D1/notes/bloostagram/app.js
@@ -10,6 +10,28 @@ const app = express();
 
 app.use(express.json());
 
+app.use(express.static("assets/css"));
+app.use(express.static("assets/images"));
+app.use("/just-scripts", express.static("assets/js"));
+
+app.get("/", (req, res) => {
+  res.send(
+    `<!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="/index.css">
+        <title>April '24</title>
+      </head>
+      <body>
+        <h1>Hello World</h1>
+        <img class='demo-img' src='/schema.png'/>
+      </body>
+    </html>`
+  );
+});
+
 app.use("/users", usersRouter);
 app.use("/albums", albumsRouter);
 app.use("/aggregates", aggregatesRouter);
